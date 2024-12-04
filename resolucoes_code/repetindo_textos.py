3625690775277c7ebaf1de7e69c0134b357f1c01
vini230403 <vini2003.melo@gmail.com>
2024-12-03 21:45:36 -0300

    execicio 2

---
+++ resolucoes_code/repetindo_textos.py
@@ -0,0 +1,10 @@
+#Solicita os dados
+num = int(input ("Digite quantas vezes será retornada a palavra: "))
+dado = input ("Digite a palavra: ")
+i = 0
+
+#Execução do código
+for i in range (0, num):
+    print (dado) 
+      
+
